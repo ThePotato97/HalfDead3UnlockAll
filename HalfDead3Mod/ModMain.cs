733be43ddeff58b8f846d95149d14c67d7d93708
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2022-10-27 04:26:06 +0100

    Add project files.

---
+++ HalfDead3Mod/ModMain.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace HalfDead3Mod
+{
+    public class ModMain : MelonMod
+    {
+
+        [HarmonyPatch(typeof(AchievementManager), "get_beta_num")]
+        private class GetBetaNumberPatch
+        {
+            private static bool Prefix(ref int __result)
+            {
+                __result = 30;
+                return false;
+            }
+        }
+    }
+}
---
+++ HalfDead3Mod/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using HalfDead3Mod;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HalfDead3Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HalfDead3Mod")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(ModMain), "Inventory Unlocker", "0.0.1", "ThePotato")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8a3de75-1357-431b-8632-37b56332fade")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
